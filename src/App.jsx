b41a90add577453baae063322423056c67f1b07c
Pedro <pedroluismartingallego@gmail.com>
2023-12-03 10:40:12 +0100

    Fin

--- src/App.jsx
+++ src/App.jsx
@@ -2,25 +2,20 @@ import { useState } from 'react';
 import './App.css';
 
 function App() {
-  let peopleYouLike = 0;
-  const [imgContainer, setImgContainer] = useState('');
-  const people = [
-    1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21,
-    22, 23, 24,
-  ];
+  const [peopleYouLike, setPeopleYouLike] = useState(0);
+  const [people, setPeople] = useState(
+    [
+      1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21,
+      22, 23, 24,
+    ].sort(() => Math.random() - 0.5)
+  );
 
-  const mixArray = (array) => {
-    return array.sort(() => Math.random() - 0.5);
-  };
-
-  mixArray(people);
+  let tempPeople = [...people];
 
   const changePerson = () => {
-    mixArray(people);
-    imgContainer(
-      (setImgContainer.style.backgroundImage = `url('img/${people[0]}.png')`)
-    );
-    if (people.length === 0) {
+    tempPeople.shift();
+    setPeople(tempPeople);
+    if (tempPeople.length === 0) {
       const answer = confirm('Se acabo la gente! ¿Quieres reiniciar?');
       if (answer === true) {
         location.reload();
@@ -29,25 +24,26 @@ function App() {
   };
 
   const personsYouLike = () => {
-    if (people.length === 0) {
+    if (tempPeople.length === 0) {
       return false;
     }
-    peopleYouLike++;
-    people.shift();
+    setPeopleYouLike(peopleYouLike + 1);
     changePerson();
   };
 
   const personsYouDontLike = () => {
-    if (people.length === 0) {
+    if (tempPeople.length === 0) {
       return false;
     }
-    people.shift();
     changePerson();
   };
 
   return (
     <>
-      <div className="img-container"></div>
+      <div
+        className="img-container"
+        style={{ backgroundImage: `url('img/${people[0]}.png')` }}
+      />
       <div className="container-buttom">
         <input
           type="button"
